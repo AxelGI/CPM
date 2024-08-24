2135ea57bc21d81fe5a48aaf1ff27c28b3d60aa9
Axel García <axelvelezg@gmail.com>
2024-08-24 13:27:49 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navList = document.getElementById('nav-list');
+        // Espera a que el contenido del DOM se cargue completamente
+        document.addEventListener('DOMContentLoaded', () => {
+            // Selecciona todos los enlaces del menú
+            const menuLinks = document.querySelectorAll('.nav-list a');
+            
+            // Función para ocultar el menú
+            function closeMenu() {
+                document.querySelector('.nav-list').classList.remove('active');
+            }
 
-    menuToggle.addEventListener('click', function() {
-        navList.classList.toggle('active');
-    });
-});
+            // Añade un evento de clic a cada enlace del menú
+            menuLinks.forEach(link => {
+                link.addEventListener('click', closeMenu);
+            });
+
+            // Función para manejar el menú desplegable en móviles
+            document.querySelector('.menu-toggle').addEventListener('click', () => {
+                document.querySelector('.nav-list').classList.toggle('active');
+            });
+        });
+
